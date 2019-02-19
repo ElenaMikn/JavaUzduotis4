d5cd6ab288feb00c67ca02ba8aeb5c3fe46e7494
elena <elena.markovskaja@gmail.com>
2019-02-19 19:28:04 +0200

    konstruktoriaus testai

--- src/test/java/KMItest.java
+++ src/test/java/KMItest.java
@@ -1,20 +1,38 @@
 import org.junit.Test;
-import org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 public class KMItest {
     @Test
     public void KlaseSusikuria() {
         KMI kmi = new KMI();
+        assertNotNull(kmi);
     }
 
     @Test
     public void KlaseSusikuriaSuPrametrais() {
-        KMI kmi = new KMI(180.0, 180.0, true);
+        KMI kmi = new KMI(180.0, 160.0, true);
+        assertNotNull(kmi);
+    }
+    @Test
+    public void KlaseSusikuriaSuPrametraisPatikrinamParametrus() {
+        KMI kmi = new KMI(180.0, 160.0, true);
+        assertEquals(kmi.getUgis(), 180,0);
+        assertEquals(kmi.getSvoris(), 160,0);
+        assertTrue(kmi.isMoteris());
+
+    }
+    @Test
+    public void KlaseSusikuriaBeParametruPriskiriamVeliau() {
+        KMI kmi = new KMI();
+
+        kmi.setUgis(180);
+        assertEquals(kmi.getUgis(),180,0);
+
+        kmi.setSvoris(160);
+        assertEquals(kmi.getSvoris(),160,0);
+
+        kmi.setMoteris(false);
+        assertFalse(kmi.isMoteris());
     }
 
-    @Test
-    public  void KlaseNesusikuriaSuvienuParametru() {
-        //KMI kmi = new KMI(180);
-        //assertNotNull(kmi);
-    }
 }
