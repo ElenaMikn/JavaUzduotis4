09430500de440453672c1a4212d400e769fe2068
elena <elena.markovskaja@gmail.com>
2019-02-19 21:12:00 +0200

    Padaryta main funkcija

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        double svoris;
+        double ugis;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Iveskite svori:");
+        svoris =  Double.parseDouble(scan.nextLine());
+        System.out.println("Iveskite ugi:");
+        ugis =  Double.parseDouble(scan.nextLine());
+        KMI kmi = new KMI( ugis,svoris, false);
+        double indeksas= kmi.skaiciuoti();
+        System.out.println("Jusu indeksas yra:"+indeksas);
+        System.out.println("Jusu kategorija yra:"+kmi.GetKategorija());
 
     }
 }
